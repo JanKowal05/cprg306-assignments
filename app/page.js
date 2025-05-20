480757a06ad2f818f22fafe6154b36443fbfce94
JanKowal05 <jan.kowal@edu.sait.ca>
2025-05-20 13:30:45 -0600

    Fixed Error in main page

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Home(){
   return(
     <main>
-      <h className="text-2xl align">CPRG 306: Web Development 2 - Assignments</h>
-      <br/>
+      <h1 className="text-2xl align">CPRG 306: Web Development 2 - Assignments</h1>
       <ul>
         <li><Link className="hover:text-green-400 hover:underline" href="/week-2/">Week 2 Assignment</Link></li>
         <li><Link className="hover:text-green-400 hover:underline" href="/week-3/">Week 3 Assignment</Link></li>    
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -5,7 +5,6 @@ export default function Page(){
     <main className="bg-slate-950">
         <h1 className="text-3xl font-bold m-2">Shopping List</h1>
         <ItemList/>
-
     </main>
    );
 }
