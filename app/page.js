fc63cdd52f6a2e28187bc5792db02e9d1b5f639b
JanKowal05 <jan.kowal@edu.sait.ca>
2025-05-20 13:27:35 -0600

    Finished Week 3 assignment

--- app/page.js
+++ app/page.js
@@ -5,10 +5,10 @@ export default function Home(){
     <main>
       <h className="text-2xl align">CPRG 306: Web Development 2 - Assignments</h>
       <br/>
-      <Link 
-      className="hover:text-green-400 hover:underline"
-      href="/week-2/"
-      >Week 2 Assignment</Link>
-    </main>
+      <ul>
+        <li><Link className="hover:text-green-400 hover:underline" href="/week-2/">Week 2 Assignment</Link></li>
+        <li><Link className="hover:text-green-400 hover:underline" href="/week-3/">Week 3 Assignment</Link></li>    
+      </ul>
+     </main>
   );
 }
