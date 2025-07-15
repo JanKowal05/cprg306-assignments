d4099be8fa125b8424381e954352710329f57b66
JanKowal05 <jan.kowal@edu.sait.ca>
2025-07-14 20:50:41 -0600

    Started week 9 assignment

---
+++ app/week-9/_utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3CqrxdeXUAwOSHGMtkcji43zYQ7VKBc0",
+  authDomain: "cprg306-assignments-ddf7b.firebaseapp.com",
+  projectId: "cprg306-assignments-ddf7b",
+  storageBucket: "cprg306-assignments-ddf7b.firebasestorage.app",
+  messagingSenderId: "257006505956",
+  appId: "1:257006505956:web:121b871b716e46008ac1f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
